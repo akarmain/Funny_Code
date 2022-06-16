0efa7944dbb38c9d2c833ea29922083f8cc3dc91
Andrey <12123>
2022-06-16 15:48:24 +0300

    None

--- Random word.py
+++ Random word.py
@@ -1,18 +1,17 @@
 import random
-import secrets
 
 
 def main():
-	leters = ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','y','v','w','x','u','z']
-	len_leters = random.randint(0,len(leters))
-	while len_leters>15:
-		len_leters = random.randint(0,len(leters))
-	for i in range(len_leters):
-		print(random.choice(leters), end = "")
-	print(random.choice(leters))
-
+    leters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'y',
+              'v', 'w', 'x', 'u', 'z']
+    len_leters = random.randint(0, len(leters))
+    while len_leters > 15:
+        len_leters = random.randint(0, len(leters))
+    for i in range(len_leters):
+        print(random.choice(leters), end="")
+    print(random.choice(leters))
 
 
 if __name__ == "__main__":
     main()
-    print("Randon word")
+    print("Random word")
